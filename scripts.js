eca0fd1503d78398d3eba7f23d3f9ed54bb8994a
Gabriel Dutra <ga.acdutra@gmail.com>
2022-06-26 15:12:51 -0300

    versão com envio de mensagens

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 let nome;
+let mensagem;
+let lista_mensagens = [];
 function obtemNome() {
   if (!nome) {
     nome = {
@@ -46,28 +48,57 @@ function carregamento3Segundos(mensagem) {
   const IDInterval = setInterval(carregaMensagens, 3000);
 }
 
+let a = "";
+let b = "";
+let auxiliar = 0;
 function renderizaMensagens(mensagens) {
-  console.log(mensagens.data);
   document.querySelector(".conteudo").innerHTML = "";
+  lista_mensagens = [];
+
+  // Alimenta array lista_msg
+
   for (i = 0; i < 100; i++) {
     if (mensagens.data[i].type === "status") {
-      document.querySelector(".conteudo").innerHTML += `<li class="msg_status">
+      mensagem = `<li class="msg_status">
       <div>
       <span class="horario">(${mensagens.data[i].time})</span><span class="espaço">l</span><span class="usuario">${mensagens.data[i].from}</span><span class="espaço">l</span> entra na sala... </div></li>`;
-      document.querySelector(".msg_status").scrollIntoView();
+      lista_mensagens.push(mensagem);
     }
     if (mensagens.data[i].type === "message") {
-      document.querySelector(
-        ".conteudo"
-      ).innerHTML += `<li class="msg_comum"><div><span class="horario">(${mensagens.data[i].time})</span><span class="espaço">l</span><span class="usuario">${mensagens.data[i].from}</span><span class="espaço">l</span>para<span class="espaço">l</span><span class="usuario">${mensagens.data[i].to}</span><span>:</span><span class="espaço">l</span><span class="message">${mensagens.data[i].text}</span> </div></li>`;
-      document.querySelector(".msg_comum").scrollIntoView();
+      mensagem = `<li class="msg_comum"><div><span class="horario">(${mensagens.data[i].time})</span><span class="espaço">l</span><span class="usuario">${mensagens.data[i].from}</span><span class="espaço">l</span>para<span class="espaço">l</span><span class="usuario">${mensagens.data[i].to}</span><span>:</span><span class="espaço">l</span><span class="message">${mensagens.data[i].text}</span> </div></li>`;
+      lista_mensagens.push(mensagem);
     }
     if (mensagens.data[i].type === "private_message") {
-      document.querySelector(
-        ".conteudo"
-      ).innerHTML += `<li class="msg_reservada"><span class="horario">(${mensagens.data[i].time})</span><span class="espaço">l</span><span class="usuario">${mensagens.data[i].from}</span><span class="espaço">l</span>reservadamente para<span class="espaço">l</span><span class="usuario">${mensagens.data[i].to}</span><span>:</span><span class="espaço">l</span><span class="message">${mensagens.data[i].text}</span> </li>`;
-      document.querySelector(".msg_reservada").scrollIntoView();
+      mensagem = `<li class="msg_reservada"><div><span class="horario">(${mensagens.data[i].time})</span><span class="espaço">l</span><span class="usuario">${mensagens.data[i].from}</span><span class="espaço">l</span>reservadamente para<span class="espaço">l</span><span class="usuario">${mensagens.data[i].to}</span><span>:</span><span class="espaço">l</span><span class="message">${mensagens.data[i].text}</span> </div></li>`;
+      if (mensagens.data[i].to === nome.name) {
+        lista_mensagens.push(mensagem);
+      } else {
+        i--;
+      }
     }
+  }
+
+  // Renderiza na tela as mensagens
+  for (i = 0; i < lista_mensagens.length; i++) {
+    document.querySelector(".conteudo").innerHTML += lista_mensagens[i];
+  }
+  document.querySelector(
+    ".conteudo"
+  ).innerHTML += `<div class="completa_margem"></div>`;
+  scrollUltimaMsg();
+}
+
+function scrollUltimaMsg() {
+  const ultima_msg = document.querySelector(".conteudo li:nth-last-child(2)");
+  if (auxiliar === 0) {
+    a = ultima_msg.innerHTML;
+    auxiliar++;
+  } else if (auxiliar === 1) {
+    b = ultima_msg.innerHTML;
+    auxiliar--;
+  }
+  if (a !== b) {
+    ultima_msg.scrollIntoView();
   }
 }
 
@@ -76,5 +107,34 @@ function mantemConexao() {
     "https://mock-api.driven.com.br/api/v6/uol/status",
     nome
   );
-  console.log("está conectado");
 }
+
+function enviaMensagem() {
+  if (document.querySelector(".mensagem").value !== "") {
+    const envio = {
+      from: nome.name,
+      to: "Todos",
+      text: document.querySelector(".mensagem").value,
+      type: "message",
+    };
+
+    const promise = axios.post(
+      "https://mock-api.driven.com.br/api/v6/uol/messages",
+      envio
+    );
+    promise.then(carregaMensagens);
+    promise.catch(recarregaPagina);
+  } else {
+    alert("Você não pode enviar uma mensagem vazia!");
+  }
+}
+
+function recarregaPagina() {
+  window.location.reload();
+}
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    enviaMensagem();
+  }
+});
